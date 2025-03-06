5746d3f006799d2b0e092f9a5d70f18ef92264c8
Marco Herrera <mherrerasa@sunat.gob.pe>
2025-03-06 12:22:47 -0500

    configuración incial realizado con Spring Initializr

---
+++ src/main/java/com/pe/restaurante/productos/tx/backend/RestauranteProductosApplication.java
@@ -0,0 +1,13 @@
+package com.pe.restaurante.productos.tx.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pe/restaurante/productos/tx/backend/RestauranteProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.restaurante.productos.tx.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
